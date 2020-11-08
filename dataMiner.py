aa1ee7af4191b9d94405526178e0b4bf3dd47750
stackOvercrow <gabriel-reis1999@hotmail.com>
2020-11-08 14:09:18 -0300

    bugs fixed

--- dataMiner.py
+++ dataMiner.py
@@ -14,36 +14,54 @@ for index, column in csv_file.iteritems():
     instances = 0
     instances_antecedent_column = 0
     position = 0
+    i = 0
 
     first_column = csv_file[list_of_columns[count]]
-    second_column = csv_file[list_of_columns[count+1]]
-
     list_first_column = list(first_column)
-    list_second_column = list(second_column)
 
-    print("Checando suporte entre " + list_of_columns[count] + " e " + list_of_columns[count + 1])
+    while i < len(list_of_columns):
+        print("Checando suporte entre " + list_of_columns[count] + " e " + list_of_columns[i + 1])
 
-    for element in list_first_column:
-        if list_first_column[position] == 'sim':
-            instances_antecedent_column += 1
+        second_column = csv_file[list_of_columns[i + 1]]
+        list_second_column = list(second_column)
 
-            if list_second_column[position] == 'sim':
-                instances += 1
+        for element in list_first_column:
+            if list_first_column[position] == 'sim':
+                instances_antecedent_column += 1
 
-        position += 1
+                if list_second_column[position] == 'sim':
+                    instances += 1
 
-    print("Suporte entre " + list_of_columns[count] + " e " + list_of_columns[count + 1] + " é de " + str(instances/number_of_rows)
-          + "(" + str((instances/number_of_rows) * 100) + "%)")
-    file.write("Suporte entre " + list_of_columns[count] + " e " + list_of_columns[count + 1] + " é de " + str(instances/number_of_rows)
-          + "(" + str((instances/number_of_rows) * 100) + "%)\n")
+            position += 1
 
-    print("Confiança entre " + list_of_columns[count] + " e " + list_of_columns[count + 1] + " é de " + str(instances/instances_antecedent_column)
-          + "(" + str((instances/number_of_rows) * 100) + "%)")
-    file.write("Confiança entre " + list_of_columns[count] + " e " + list_of_columns[count + 1] + " é de " + str(instances/instances_antecedent_column)
-          + "(" + str((instances/number_of_rows) * 100) + "%)\n")
-    print("\n\n\n")
-    file.write("\n")
-    
+        position = 0
+
+        print(instances)
+
+        print("Suporte entre " + list_of_columns[count] + " e " + list_of_columns[i + 1] + " é de " + str(
+            instances / number_of_rows)
+              + "(" + str((instances / number_of_rows) * 100) + "%)")
+        file.write("Suporte entre " + list_of_columns[count] + " e " + list_of_columns[i + 1] + " é de " + str(
+            instances / number_of_rows)
+                   + "(" + str((instances / number_of_rows) * 100) + "%)\n")
+
+        print("Confiança entre " + list_of_columns[count] + " e " + list_of_columns[i + 1] + " é de " + str(
+            instances / instances_antecedent_column)
+              + "(" + str((instances / number_of_rows) * 100) + "%)")
+        file.write("Confiança entre " + list_of_columns[count] + " e " + list_of_columns[i + 1] + " é de " + str(
+            instances / instances_antecedent_column)
+                   + "(" + str((instances / number_of_rows) * 100) + "%)\n")
+        print("\n\n\n")
+        file.write("\n")
+
+        if i < (len(list_of_columns) - 2):
+            i += 1
+        else:
+            break
+
+        instances = 0
+        instances_antecedent_column = 0
+
     if count < (len(list_of_columns) - 2):
         count += 1
 
